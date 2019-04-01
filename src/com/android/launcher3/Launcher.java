3fcf83662dbb9630c2145ba9ef0b6fc5eb2d28c0
Amir Zaidi <azaidi@live.nl>
2019-04-01 16:24:14 +0200

    SparkLauncher: Extend notification listener with proxy
    Change-Id: If6bb3d4a5c25deee8994044e0d99311d4ffad906

--- src/com/android/launcher3/Launcher.java
+++ src/com/android/launcher3/Launcher.java
@@ -477,6 +477,7 @@ public class Launcher extends StatefulActivity<LauncherState> implements Launche
         setupViews();
         crossFadeWithPreviousAppearance();
         mPopupDataProvider = new PopupDataProvider(this::updateNotificationDots);
+        LauncherNotifications.getInstance().addListener(mPopupDataProvider);
 
         boolean internalStateHandled = ACTIVITY_TRACKER.handleCreate(this);
         if (internalStateHandled) {
@@ -989,7 +990,7 @@ public class Launcher extends StatefulActivity<LauncherState> implements Launche
         mModel.validateModelDataOnResume();
 
         // Set the notification listener and fetch updated notifications when we resume
-        NotificationListener.setNotificationsChangedListener(mPopupDataProvider);
+        NotificationListener.setNotificationsChangedListener(LauncherNotifications.getInstance());
 
         DiscoveryBounce.showForHomeIfNeeded(this);
         mAppWidgetHost.setActivityResumed(true);
