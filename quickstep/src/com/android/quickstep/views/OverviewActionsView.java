4a69dfe672259812599631f16aa25e70a16ac21c
Pranav Vashi <neobuddy89@gmail.com>
2022-02-13 03:38:19 +0530

    SparkLauncher: Make lens available without lens package
    * Google search package is capable of lens functionality. Who knew? :)
    
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- quickstep/src/com/android/quickstep/views/OverviewActionsView.java
+++ quickstep/src/com/android/quickstep/views/OverviewActionsView.java
@@ -36,6 +36,7 @@ import com.android.launcher3.R;
 import com.android.launcher3.uioverrides.ApiWrapper;
 import com.android.launcher3.util.DisplayController;
 import com.android.launcher3.util.DisplayController.NavigationMode;
+import com.android.launcher3.Utilities;
 import com.android.launcher3.util.MultiValueAlpha;
 import com.android.launcher3.util.MultiValueAlpha.AlphaProperty;
 import com.android.quickstep.TaskOverlayFactory.OverlayUICallbacks;
@@ -116,7 +117,7 @@ public class OverviewActionsView<T extends OverlayUICallbacks> extends FrameLayo
         super.onFinishInflate();
         findViewById(R.id.action_screenshot).setOnClickListener(this);
         findViewById(R.id.action_clear_all).setOnClickListener(this);
-        if (getContext().getPackageManager().getLaunchIntentForPackage("com.google.ar.lens") != null) {
+        if (Utilities.isGSAEnabled(getContext())) {
             View lens = findViewById(R.id.action_lens);
             findViewById(R.id.action_lens).setOnClickListener(this);
             lens.setVisibility(VISIBLE);
