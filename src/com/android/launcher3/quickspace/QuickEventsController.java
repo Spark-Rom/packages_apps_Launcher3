GraveyardDrift/android_packages_apps_Launcher3

Quickspace: Refactor/clean up for 12L
* Refactor according to our liking
* Drop OmnijawClient and quickevent checks
* Use double line by default
* Make it look like latest pixel's "At a glance"
* Code cleanup and fixes by @ neobuddy89
* Remove deprecated parts (shadow, bubble etc)

Co-authored-by: Pranav Vashi <neobuddy89@gmail.com>
Signed-off-by: minarypenguin <alexfinhart@gmail.com>

src/com/android/launcher3/quickspace/QuickEventsController.java
@@ -15,32 +15,26 @@
  */
 package com.android.launcher3.quickspace;
 
-import android.app.SearchManager;
 import android.content.ActivityNotFoundException;
 import android.content.Context;
 import android.content.BroadcastReceiver;
 import android.content.Intent;
 import android.content.IntentFilter;
 import android.content.SharedPreferences;
-import android.os.SystemProperties;
 import android.provider.Settings;
 import android.view.View;
 import android.view.View.OnClickListener;
 
 import com.android.launcher3.Launcher;
 import com.android.launcher3.LauncherFiles;
-import com.android.launcher3.Utilities;
 import com.android.launcher3.R;
+import com.android.launcher3.Utilities;
 
 import java.util.Calendar;
-import java.util.List;
 import java.util.Random;
-import java.util.ArrayList;
 
 public class QuickEventsController {
 
-    // private static final int AMBIENT_INFO_MAX_DURATION = 120000; // 2 minutes
-    private static final String SETTING_DEVICE_INTRO_COMPLETED = "device_introduction_completed";
     private Context mContext;
 
     private String mEventTitle;
@@ -48,35 +42,20 @@ public class QuickEventsController {
     private OnClickListener mEventTitleSubAction = null;
     private int mEventSubIcon;
 
-    private boolean mIsQuickEvent = false;
     private boolean mRunning = true;
     private boolean mRegistered = false;
 
-    // Device Intro
-    private boolean mEventIntro = false;
-    private boolean mIsFirstTimeDone = false;
-    private SharedPreferences mPreferences;
-
     // PSA + Personality
-    private boolean mEventPSA = false;
-    private String mPSAMessage;
-    private Calendar mPSACalendar = Calendar.getInstance();
-    private int mPSARandom;
-    private Random mPSAGenerator;
     private String[] mPSAMorningStr;
     private String[] mPSAEvenStr;
     private String[] mPSAMidniteStr;
     private String[] mPSARandomStr;
+    private String[] mPSATitleStr;
     private BroadcastReceiver mPSAListener = new BroadcastReceiver() {
         @Override
         public void onReceive(Context context, Intent intent) {
-            Calendar mPSAOldCalendar = mPSACalendar;
-            mPSACalendar = Calendar.getInstance();
-            if (mPSACalendar != mPSAOldCalendar) {
-                psonalityEvent();
-            }
+            psonalityEvent();
         }
-
     };
 
     public QuickEventsController(Context context) {
@@ -85,8 +64,6 @@ public class QuickEventsController {
     }
 
     public void initQuickEvents() {
-        mPreferences = mContext.getSharedPreferences(LauncherFiles.SHARED_PREFERENCES_KEY, Context.MODE_PRIVATE);
-        mIsFirstTimeDone = mPreferences.getBoolean(SETTING_DEVICE_INTRO_COMPLETED, false);
         registerPSAListener();
         updateQuickEvents();
     }
@@ -108,30 +85,19 @@ public class QuickEventsController {
     }
 
     public void updateQuickEvents() {
-        deviceIntroEvent();
         psonalityEvent();
     }
 
-    private void deviceIntroEvent() {
+    public void psonalityEvent() {
         if (!mRunning) return;
-
-        if (mIsFirstTimeDone) {
-            mEventIntro = false;
-            return;
-        }
-        mIsQuickEvent = true;
-        mEventIntro = true;
-        mEventTitle = mContext.getResources().getString(R.string.quick_event_rom_intro_welcome);
-        mEventTitleSub = mContext.getResources().getStringArray(R.array.welcome_message_variants)[getLuckyNumber(0,6)];
+        
+        mEventTitle = mContext.getResources().getStringArray(R.array.welcome_message_variants)[getLuckyNumber(0,10)];
+        mEventTitleSub = mContext.getResources().getStringArray(R.array.quickspace_psa_random)[getLuckyNumber(0,22)];
         mEventSubIcon = R.drawable.ic_quickspace_derp;
 
         mEventTitleSubAction = new OnClickListener() {
             @Override
             public void onClick(View view) {
-                mContext.getSharedPreferences(LauncherFiles.SHARED_PREFERENCES_KEY, Context.MODE_PRIVATE)
-                        .edit()
-                        .putBoolean(SETTING_DEVICE_INTRO_COMPLETED, true)
-                        .commit();
                 Intent intent = new Intent(Intent.ACTION_MAIN);
                 intent.addCategory(Intent.CATEGORY_HOME);
                 intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_RESET_TASK_IF_NEEDED);
@@ -139,17 +105,9 @@ public class QuickEventsController {
                     Launcher.getLauncher(mContext).startActivitySafely(view, intent, null);
                 } catch (ActivityNotFoundException ex) {
                 }
-                mIsQuickEvent = false;
             }
         };
-    }
 
-    public void psonalityEvent() {
-        if (mEventIntro || mEventNowPlaying) return;
-
-        if (!Utilities.isQuickspacePersonalityEnabled(mContext)) return;
-
-        mEventTitle = Utilities.formatDateTime(mContext, System.currentTimeMillis());
         mPSAMorningStr = mContext.getResources().getStringArray(R.array.quickspace_psa_morning);
         mPSAEvenStr = mContext.getResources().getStringArray(R.array.quickspace_psa_evening);
         mPSAMidniteStr = mContext.getResources().getStringArray(R.array.quickspace_psa_midnight);
@@ -164,54 +122,35 @@ public class QuickEventsController {
             }
         };
 
-        switch (mPSACalendar.get(Calendar.HOUR_OF_DAY)) {
-            case 5: case 6: case 7: case 8: case 9:
+        switch (Calendar.getInstance().get(Calendar.HOUR_OF_DAY)) {
+            case 5: case 6: case 7: case 8: case 9: case 10:
                 psaLength = mPSAMorningStr.length - 1;
                 mEventTitleSub = mPSAMorningStr[getLuckyNumber(0, psaLength)];
                 mEventSubIcon = R.drawable.ic_quickspace_morning;
-                mIsQuickEvent = true;
-                mEventPSA = true;
                 break;
 
-            case 19: case 20: case 21: case 22: case 23: case 0:
+            case 18: case 19: case 20: case 21: case 22: case 23:
                 psaLength = mPSAEvenStr.length - 1;
                 mEventTitleSub = mPSAEvenStr[getLuckyNumber(0, psaLength)];
                 mEventSubIcon = R.drawable.ic_quickspace_evening;
-                mIsQuickEvent = true;
-                mEventPSA = true;
                 break;
 
-            case 1: case 2: case 3: case 4:
-                psaLength = mPSAEvenStr.length - 1;
-                mEventTitleSub = mPSAEvenStr[getLuckyNumber(0, psaLength)];
-                mEventSubIcon = R.drawable.ic_quickspace_evening;
-                mIsQuickEvent = true;
-                mEventPSA = true;
+            case 0: case 1: case 2: case 3: case 4:
+                psaLength = mPSAMidniteStr.length - 1;
+                mEventTitleSub = mPSAMidniteStr[getLuckyNumber(0, psaLength)];
+                mEventSubIcon = R.drawable.ic_quickspace_midnight;
+                break;
+                
+            case 11: case 12: case 13: case 14: case 15: case 16: case 17:
+                psaLength = mPSARandomStr.length - 1;
+                mEventTitleSub = mPSARandomStr[getLuckyNumber(0, psaLength)];
+                mEventSubIcon = R.drawable.ic_quickspace_derp;
                 break;
 
             default:
-                if (getLuckyNumber(13) == 7) {
-                    psaLength = mPSARandomStr.length - 1;
-                    mEventTitleSub = mPSARandomStr[getLuckyNumber(0, psaLength)];
-                    mEventSubIcon = R.drawable.ic_quickspace_derp;
-                    mIsQuickEvent = true;
-                    mEventPSA = true;
-                } else {
-                    mIsQuickEvent = false;
-                    mEventPSA = false;
-                }
                 break;
-        }
-
-    }
-
-    public boolean isQuickEvent() {
-        return mIsQuickEvent;
-    }
-
-    public boolean isDeviceIntro() {
-        return mEventIntro;
-    }
+      }
+   }
 
     public String getTitle() {
         return mEventTitle;

Spark-Rom/packages_apps_Launcher3

Change-Id: I168e06b628f1bf902622b53ecf527d0271e4180e

src/com/android/launcher3/quickspace/QuickEventsController.java
@@ -15,32 +15,26 @@
  */
 package com.android.launcher3.quickspace;
 
-import android.app.SearchManager;
 import android.content.ActivityNotFoundException;
 import android.content.Context;
 import android.content.BroadcastReceiver;
 import android.content.Intent;
 import android.content.IntentFilter;
 import android.content.SharedPreferences;
-import android.os.SystemProperties;
 import android.provider.Settings;
 import android.view.View;
 import android.view.View.OnClickListener;
 
 import com.android.launcher3.Launcher;
 import com.android.launcher3.LauncherFiles;
-import com.android.launcher3.Utilities;
 import com.android.launcher3.R;
+import com.android.launcher3.Utilities;
 
 import java.util.Calendar;
-import java.util.List;
 import java.util.Random;
-import java.util.ArrayList;
 
 public class QuickEventsController {
 
-    // private static final int AMBIENT_INFO_MAX_DURATION = 120000; // 2 minutes
-    private static final String SETTING_DEVICE_INTRO_COMPLETED = "device_introduction_completed";
     private Context mContext;
 
     private String mEventTitle;
@@ -48,35 +42,20 @@ public class QuickEventsController {
     private OnClickListener mEventTitleSubAction = null;
     private int mEventSubIcon;
 
-    private boolean mIsQuickEvent = false;
     private boolean mRunning = true;
     private boolean mRegistered = false;
 
-    // Device Intro
-    private boolean mEventIntro = false;
-    private boolean mIsFirstTimeDone = false;
-    private SharedPreferences mPreferences;
-
     // PSA + Personality
-    private boolean mEventPSA = false;
-    private String mPSAMessage;
-    private Calendar mPSACalendar = Calendar.getInstance();
-    private int mPSARandom;
-    private Random mPSAGenerator;
     private String[] mPSAMorningStr;
     private String[] mPSAEvenStr;
     private String[] mPSAMidniteStr;
     private String[] mPSARandomStr;
+    private String[] mPSATitleStr;
     private BroadcastReceiver mPSAListener = new BroadcastReceiver() {
         @Override
         public void onReceive(Context context, Intent intent) {
-            Calendar mPSAOldCalendar = mPSACalendar;
-            mPSACalendar = Calendar.getInstance();
-            if (mPSACalendar != mPSAOldCalendar) {
-                psonalityEvent();
-            }
+            psonalityEvent();
         }
-
     };
 
     public QuickEventsController(Context context) {
@@ -85,8 +64,6 @@ public class QuickEventsController {
     }
 
     public void initQuickEvents() {
-        mPreferences = mContext.getSharedPreferences(LauncherFiles.SHARED_PREFERENCES_KEY, Context.MODE_PRIVATE);
-        mIsFirstTimeDone = mPreferences.getBoolean(SETTING_DEVICE_INTRO_COMPLETED, false);
         registerPSAListener();
         updateQuickEvents();
     }
@@ -108,30 +85,19 @@ public class QuickEventsController {
     }
 
     public void updateQuickEvents() {
-        deviceIntroEvent();
         psonalityEvent();
     }
 
-    private void deviceIntroEvent() {
+    public void psonalityEvent() {
         if (!mRunning) return;
-
-        if (mIsFirstTimeDone) {
-            mEventIntro = false;
-            return;
-        }
-        mIsQuickEvent = true;
-        mEventIntro = true;
-        mEventTitle = mContext.getResources().getString(R.string.quick_event_rom_intro_welcome);
-        mEventTitleSub = mContext.getResources().getStringArray(R.array.welcome_message_variants)[getLuckyNumber(0,6)];
+        
+        mEventTitle = mContext.getResources().getStringArray(R.array.welcome_message_variants)[getLuckyNumber(0,10)];
+        mEventTitleSub = mContext.getResources().getStringArray(R.array.quickspace_psa_random)[getLuckyNumber(0,22)];
         mEventSubIcon = R.drawable.ic_quickspace_corvus;
 
         mEventTitleSubAction = new OnClickListener() {
             @Override
             public void onClick(View view) {
-                mContext.getSharedPreferences(LauncherFiles.SHARED_PREFERENCES_KEY, Context.MODE_PRIVATE)
-                        .edit()
-                        .putBoolean(SETTING_DEVICE_INTRO_COMPLETED, true)
-                        .commit();
                 Intent intent = new Intent(Intent.ACTION_MAIN);
                 intent.addCategory(Intent.CATEGORY_HOME);
                 intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_RESET_TASK_IF_NEEDED);
@@ -139,17 +105,9 @@ public class QuickEventsController {
                     Launcher.getLauncher(mContext).startActivitySafely(view, intent, null);
                 } catch (ActivityNotFoundException ex) {
                 }
-                mIsQuickEvent = false;
             }
         };
-    }
 
-    public void psonalityEvent() {
-        if (mEventIntro || mEventNowPlaying) return;
-
-        if (!Utilities.isQuickspacePersonalityEnabled(mContext)) return;
-
-        mEventTitle = Utilities.formatDateTime(mContext, System.currentTimeMillis());
         mPSAMorningStr = mContext.getResources().getStringArray(R.array.quickspace_psa_morning);
         mPSAEvenStr = mContext.getResources().getStringArray(R.array.quickspace_psa_evening);
         mPSAMidniteStr = mContext.getResources().getStringArray(R.array.quickspace_psa_midnight);
@@ -164,54 +122,35 @@ public class QuickEventsController {
             }
         };
 
-        switch (mPSACalendar.get(Calendar.HOUR_OF_DAY)) {
-            case 5: case 6: case 7: case 8: case 9:
+        switch (Calendar.getInstance().get(Calendar.HOUR_OF_DAY)) {
+            case 5: case 6: case 7: case 8: case 9: case 10:
                 psaLength = mPSAMorningStr.length - 1;
                 mEventTitleSub = mPSAMorningStr[getLuckyNumber(0, psaLength)];
                 mEventSubIcon = R.drawable.ic_quickspace_morning;
-                mIsQuickEvent = true;
-                mEventPSA = true;
                 break;
 
-            case 19: case 20: case 21: case 22: case 23: case 0:
+            case 18: case 19: case 20: case 21: case 22: case 23:
                 psaLength = mPSAEvenStr.length - 1;
                 mEventTitleSub = mPSAEvenStr[getLuckyNumber(0, psaLength)];
                 mEventSubIcon = R.drawable.ic_quickspace_evening;
-                mIsQuickEvent = true;
-                mEventPSA = true;
                 break;
 
-            case 1: case 2: case 3: case 4:
-                psaLength = mPSAEvenStr.length - 1;
-                mEventTitleSub = mPSAEvenStr[getLuckyNumber(0, psaLength)];
-                mEventSubIcon = R.drawable.ic_quickspace_evening;
-                mIsQuickEvent = true;
-                mEventPSA = true;
+            case 0: case 1: case 2: case 3: case 4:
+                psaLength = mPSAMidniteStr.length - 1;
+                mEventTitleSub = mPSAMidniteStr[getLuckyNumber(0, psaLength)];
+                mEventSubIcon = R.drawable.ic_quickspace_midnight;
+                break;
+                
+            case 11: case 12: case 13: case 14: case 15: case 16: case 17:
+                psaLength = mPSARandomStr.length - 1;
+                mEventTitleSub = mPSARandomStr[getLuckyNumber(0, psaLength)];
+                mEventSubIcon = R.drawable.ic_quickspace_corvus;
                 break;
 
             default:
-                if (getLuckyNumber(13) == 7) {
-                    psaLength = mPSARandomStr.length - 1;
-                    mEventTitleSub = mPSARandomStr[getLuckyNumber(0, psaLength)];
-                    mEventSubIcon = R.drawable.ic_quickspace_corvus;
-                    mIsQuickEvent = true;
-                    mEventPSA = true;
-                } else {
-                    mIsQuickEvent = false;
-                    mEventPSA = false;
-                }
                 break;
-        }
-
-    }
-
-    public boolean isQuickEvent() {
-        return mIsQuickEvent;
-    }
-
-    public boolean isDeviceIntro() {
-        return mEventIntro;
-    }
+      }
+   }
 
     public String getTitle() {
         return mEventTitle;